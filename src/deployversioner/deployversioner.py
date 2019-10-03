a6691dc802643964de8b2b77d1acdc3b2434ae28
Jesper Bruun <jbr@dbc.dk>
2019-10-03 14:51:10 +0200

    Bump max files per page in get directory tree to 5000

--- src/deployversioner/deployversioner.py
+++ src/deployversioner/deployversioner.py
@@ -110,7 +110,7 @@ def change_image_tag(gitlab_request: GitlabRequest, file_object: str, image_tag:
     if url[:4] != "http":
         url = "https://{}".format(url)
     headers = {"private-token": gitlab_request.api_token}
-    url = "{}/api/v4/projects/{}/repository/tree/?ref={}&recursive=True&per_page=500".format(url,
+    url = "{}/api/v4/projects/{}/repository/tree/?ref={}&recursive=True&per_page=5000".format(url,
                                                                                 gitlab_request.project_id,
                                                                                 gitlab_request.branch)
     request = urllib.request.Request(url, headers=headers, method="GET")
