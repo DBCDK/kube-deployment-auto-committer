9c7a14c09e4abf9a14312f903c74350819d635cc
mvs <mvs@dbc.dk>
2022-07-04 15:39:00 +0200

    added guard for noneType return  in yaml.safe_load_all due to headless java services not being a complete service

--- src/deployversioner/deployversioner.py
+++ src/deployversioner/deployversioner.py
@@ -60,7 +60,9 @@ def set_image_tag(gitlab_request: GitlabRequest, filename: str,
     changed=False
     changed_image_tags = set()
     for doc in docs:
-        if "kind" in doc and doc["kind"] in ["Deployment", "StatefulSet", "CronJob", "Job"]:
+        # added guard for None type as the script would otherwise fail on headless services
+        # should not have any negative effects, as headless currently does not contain docker tag/buildnr information
+        if doc != None and "kind" in doc and doc["kind"] in ["Deployment", "StatefulSet", "CronJob", "Job"]:
             try:
                 if doc["kind"] == 'CronJob':
                     containers = doc["spec"]["jobTemplate"]["spec"]["template"]["spec"]["containers"]
